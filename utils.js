3585548ee821458061102c3664ae9f7875e0dc67
orad <orad88@gmail.com>
2023-06-16 11:06:16 +0300

    add details

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+// Orad Dostra : 208939736
+// Niv Vardi: 209083278
+
 const generateRandomId = () => {
     const timestamp = new Date().getTime();
     const randomNumber = Math.floor(Math.random() * 10000);
